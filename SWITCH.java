28a974d806e6a0578647b8552cce7e09b8db3fe0
ZIMULA ABDUL <zimulaabdul04@gmail.com>
2024-09-19 10:46:07 +0300

    Add files via upload

---
+++ SWITCH.java
@@ -0,0 +1,36 @@
+
+//A JAVA PROGRAM FOR GRADING USING SWITCH STATEMENTS
+import java.util.Scanner;
+
+public class SWITCH {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ENTER MARKS:");
+        int marks = scanner.nextInt();
+        switch (marks / 10) {
+            case 10:
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            case 5:
+                System.out.println("E");
+                break;
+            case 4:
+                System.out.println("PASS");
+                break;
+            default:
+                System.out.println("FAIL");
+        }
+        scanner.close();
+    }
+}
